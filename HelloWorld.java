fd52b2f881b23fdcb53d305c9d7890b63ac10e5d
Riddhi Chipade <riddhichipade@gmail.com>
2024-08-30 13:12:44 +0530

    new Method added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,14 @@
 public class HelloWorld {
-    static void method(){
+    static void method1(){
        System.out.println("Hello Java Method");
      System.out.println("Hello Changed Java Method");
      }
-   
+    static void method2(){
+     System.out.println("Hello in Method2");
+    }
     public static void main(String[] args) {
         System.out.println("Hello Java World");
-         method();
+         method1();
+         method2();
     }
 }
